d041602296445a55da7aebf7985b6ca09b27aa40
AvantikaSinghAVI <41646617+AvantikaSinghAVI@users.noreply.github.com>
2019-03-29 23:33:46 +0530

    Add files via upload

---
+++ mod4Solution/script.js
@@ -0,0 +1,15 @@
+var name;
+
+function msgname (name) {
+		(name[0]=='j' || name[0] == 'J') ?(console.log("GoodBye "+name)):(console.log("Hello "+name));
+ }
+
+msgname("Jyotika");
+msgname("avantika");
+msgname("jim");
+msgname("priyansha");
+msgname("Jyoti");
+msgname("Jmi");
+msgname("Bhaskar");
+msgname("chiya");
+msgname("Ankita");
